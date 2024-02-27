1097a0a8acce4643211d0d681bc82c371671d9e1
manojvsp <gmanojece2021@gmail.com>
2024-02-27 08:04:29 +0530

    styling

--- src/store/Counter.jsx
+++ src/store/Counter.jsx
@@ -7,13 +7,17 @@ const Counter = () => {
     const dispatch = useDispatch();
     const [amount,setAmount] = useState(0)
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex flex-col gap-5 h-[100vh] items-center justify-center'>
         <h1>{count}</h1>
-        <button onClick={()=>dispatch(increase())}>+</button>
-        <button onClick={()=>dispatch(decrease())}>-</button>
-        <input type="number" onChange={(e)=>setAmount(parseInt(e.target.value))} />
-        <button onClick={()=>dispatch(incrementByAmount(amount))}>ByAmount</button>
-        <button onClick={()=>dispatch(reset())}>Reset</button>
+        <div className='flex  gap-3'>
+        <button className=' border border-black-300 px-2 bg-slate-900 text-white' onClick={()=>dispatch(increase())}>+</button>
+        <button className='border border-black-300 px-2 bg-slate-900 text-white' onClick={()=>dispatch(decrease())}>-</button>
+        </div>
+        <input className='border border-black-700' type="number" onChange={(e)=>setAmount(parseInt(e.target.value))} />
+        <div className='flex gap-5'>
+        <button className='border border-black-300 p-1 bg-slate-900 text-white' onClick={()=>dispatch(incrementByAmount(amount))}>ByAmount</button>
+        <button className='border border-black-300 p-1 bg-slate-900 text-white' onClick={()=>dispatch(reset())}>Reset</button>
+        </div>
     </div>
   )
 }
